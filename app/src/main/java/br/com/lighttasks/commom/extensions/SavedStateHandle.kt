9d39f74e0784e7838a47a88579d3ad4c1e663871
Pedro Resende da Silva <pedroresende61@gmail.com>
2022-11-14 13:07:27 -0300

    initial commit

---
+++ app/src/main/java/br/com/lighttasks/commom/extensions/SavedStateHandle.kt
@@ -0,0 +1,13 @@
+package br.com.lighttasks.commom.extensions
+
+import androidx.lifecycle.SavedStateHandle
+
+fun <T> SavedStateHandle.getArgument(key: String): T? {
+    val argument = get<T>(key = key)
+    remove<T>(key = key)
+    return argument
+}
+
+fun <T> SavedStateHandle.putArgument(key: String, argument: T?) {
+    set(key = key, value = argument)
+}
---
+++ app/src/main/java/br/com/lighttasks/presentation/ui/compose/navigation/Screens.kt
@@ -0,0 +1,65 @@
+package br.com.lighttasks.presentation.ui.compose.navigation
+
+import androidx.navigation.NavHostController
+import br.com.lighttasks.commom.extensions.putArgument
+
+sealed class Screens(val route: String, val argumentKey: String) {
+
+    fun <T> passArgument(argument: T?): String {
+        return this.route.replace(oldValue = "{$argumentKey}", newValue = argument.toString())
+    }
+
+    fun backToScreen(navHostController: NavHostController) {
+        navHostController.navigate(route = route) {
+            popUpTo(0)
+        }
+    }
+
+    fun <T> navigateWithArgument(
+        navHostController: NavHostController,
+        argumentValue: T?
+    ) {
+        navHostController.currentBackStackEntry?.savedStateHandle?.putArgument(
+            key = argumentKey,
+            argument = argumentValue
+        )
+        navHostController.navigate(route)
+    }
+
+    fun <T> navigateWithListArgument(
+        navHostController: NavHostController,
+        argumentValue: List<T>?
+    ) {
+        navHostController.currentBackStackEntry?.savedStateHandle?.set(
+            key = argumentKey,
+            value = argumentValue
+        )
+        navHostController.navigate(route)
+    }
+
+
+    object Tasks : Screens(
+        route = "tasks?tasks_argument={tasks_argument}",
+        argumentKey = "tasks_argument"
+    )
+
+    object TaskDetail : Screens(
+        route = "task_detail",
+        argumentKey = "task_detail_argument"
+    )
+
+    object Groups : Screens(
+        route = "groups?groups_argument={groups_argument}",
+        argumentKey = "groups_argument"
+    )
+
+    object GroupsDetail : Screens(
+        route = "group_detail?group_detail_argument={group_detail_argument}",
+        argumentKey = "group_detail_argument"
+    )
+
+    object Profile : Screens(
+        route = "trash_can?trash_can_argument={trash_can_argument}",
+        argumentKey = "profile_argument"
+    )
+}
