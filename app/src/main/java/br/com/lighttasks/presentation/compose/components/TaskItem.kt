d243c545849511b56ebc5af625cdd943a6c3df91
Pedro Resende da Silva <pedroresende61@gmail.com>
2022-11-22 20:44:17 -0300

    clear code

--- app/src/main/java/br/com/lighttasks/presentation/compose/components/TaskItem.kt
+++ app/src/main/java/br/com/lighttasks/presentation/compose/components/TaskItem.kt
@@ -8,20 +8,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.lighttasks.commom.util.priority.getPriorityContainerColor
 import br.com.lighttasks.commom.util.priority.getPriorityOnContainerColor
-import br.com.lighttasks.domain.model.Priority
 import br.com.lighttasks.domain.model.Task
-import br.com.lighttasks.domain.model.tasks
-import br.com.lighttasks.presentation.compose.theme.LightTasksTheme
 
 @Composable
 fun TaskItem(
